e52c4a7f7b39283ad77e1e94b9913608ac4ae3db
Gabriel L Martinez <g.lorenzo.martinez@gmail.com>
2019-03-31 09:45:30 -0400

    Update app.js
    Simplify the port server assigning

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express')
 
 const app = express()
-
-const port = 3000
-
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+const server = app.listen(3000, () => {
+    console.log(`App is running on port ${server.address().port}!`);
+});
