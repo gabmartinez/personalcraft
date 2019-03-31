b33620617ff0d707a2986834ffcd467932743557
Gabriel L Martinez <g.lorenzo.martinez@gmail.com>
2019-03-31 09:48:31 -0400

    Create index.pug extended with layout.pug

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require('express')
 const app = express()
 app.set('view engine', 'pug')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Hey', message: 'Hello there!' })
+})
 
 const server = app.listen(3000, () => {
     console.log(`App is running on port ${server.address().port}!`);
